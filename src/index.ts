b5003b4cc3b8d93914609027da50297672c9d5cd
astrokachi <astrokachi@gmail.com>
2023-09-15 20:07:30 +0100

    feat: basic crud api for users

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import http from "http";
+import express from "express";
+import prisma from "./prisma/connection";
+import userRoutes from "./routes/user.route";
+import { errorHandler } from "./errors/errorHandler";
 
-const server = http.createServer();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", userRoutes);
+app.use(errorHandler);
 
+app.get("/", (req, res) => {
+	console.log("Hello World");
+});
 
-server.listen(3000, () => {
-	console.log("server is up and running");
+app.listen(3000, async () => {
+	await prisma.$connect();
+	console.log("Server is running on port 3000");
 });
